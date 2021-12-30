1fdfe3ed13f36a7e8a209d6874bee73575b1f561
albertlockett <albert.lockett@sonraisecurity.com>
2021-12-30 07:32:18 -0400

    first commit

---
+++ scylla-tools-01-ebpf/src/main.rs
@@ -0,0 +1,26 @@
+#![no_std]
+#![no_main]
+
+use aya_bpf::{
+    macros::kprobe,
+    programs::ProbeContext,
+};
+use aya_log_ebpf::debug;
+
+#[kprobe(name="scylla_tools_01")]
+pub fn scylla_tools_01(ctx: ProbeContext) -> u32 {
+    match unsafe { try_scylla_tools_01(ctx) } {
+        Ok(ret) => ret,
+        Err(ret) => ret,
+    }
+}
+
+unsafe fn try_scylla_tools_01(ctx: ProbeContext) -> Result<u32, u32> {
+    debug!(&ctx, "try scylla tools 01");
+    Ok(0)
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    unsafe { core::hint::unreachable_unchecked() }
+}
