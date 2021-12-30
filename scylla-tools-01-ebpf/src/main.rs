8044fa531e06b52caf24178649e3c98b1d12f705
albertlockett <albert.lockett@gmail.com>
2021-12-30 14:18:57 -0400

    commit

--- scylla-tools-01-ebpf/src/main.rs
+++ scylla-tools-01-ebpf/src/main.rs
@@ -25,18 +25,24 @@ pub fn scylla_tools_01(ctx: ProbeContext) -> u32 {
 
 unsafe fn try_scylla_tools_01(ctx: ProbeContext) -> Result<u32, u32> {
   let mut buf = [0u8; 64];
-  let numbread = unsafe {
+  let numreado = unsafe {
     helpers::bpf_probe_read_user_str(
       (*ctx.regs).rax as *const u8,
       &mut buf//.as_mut_ptr()// as *mut c_void,
-    )? as u32
+    )
+  }
+
+  let numread = match numreado {
+    Ok(v) => v as u32,
+    Err(_e) => -1
   };
 
   let pid = helpers::bpf_get_current_pid_tgid() as u32;
   debug!(&ctx, "thing happened");
   let file_event = FileEvent{
     pid: pid,
-    filename: buf
+    filename: buf,
+    numread: numread
   };
   EVENTS.output(&ctx, &file_event, 0);
   Ok(0)
